ff4b3d4ef30242d08fd71307bf849ebde43f62b8
Olivier Bloch <obloch@microsoft.com>
2016-06-16 10:13:51 -0700

    Updated function code

--- iothubechofunction/index.js
+++ iothubechofunction/index.js
@@ -5,30 +5,24 @@ var connectionString = process.env.AzureIoTHubConnectionString;
 
 module.exports = function (context, myEventHubTrigger) {
     var client = Client.fromConnectionString(connectionString);
-    var targetDevice = myEventHubTrigger.DeviceId;
-    var messageToSend = "whoops";
 
-    // 1. Open our Azure IoT Hub client
-    context.log('Opening client using connection string ' + connectionString);
-    client.open(function (err) {
-        if (err) {
-            context.log('Error while opening IoT Hub service client: ' + err);
-        } else {
-            context.log('Connected.');
-            
-            var messageReceived = new Message(myEventHubTrigger);
-            
-            context.log('Received new message: ' + messageReceived.message + ' from device ' + messageReceived.DeviceId);
-            context.log('Echoing message back.');
-            // 2. Generate Message for our device
-//            var data = JSON.stringify({ message: "Hello" });
-//            var message = new Message(data);
-    
-            // 3. Send message to device
-            context.log('Sending message to ' + messageReceived.DeviceId + ' : ' + messageReceived.getData());
-            client.send(messageReceived.DeviceId, messageReceived, printResultFor('send', context));
-        }
-    });
+    if (!myEventHubTrigger) {
+        context.log("Function triggered but without content. Discarding.");
+    }
+    else {
+        context.log ("Function triggered by device " + myEventHubTrigger.userId + " with following content: " + myEventHubTrigger.getData());
+        context.log('Opening IoT Hub client using connection string ' + connectionString);
+        client.open(function (err) {
+            if (err) {
+                context.log('Error while opening IoT Hub service client: ' + err);
+            } else {
+                context.log('Connected.');
+                var messageToSend = new Message(myEventHubTrigger.getData());
+                context.log('Echoing message back to ' + myEventHubTrigger.userId + ' : ' + messageToSend.getData());
+                client.send(myEventHubTrigger.userId, messageToSend, printResultFor('send', context));
+            }
+        });
+    }
     context.done();
 };
 
