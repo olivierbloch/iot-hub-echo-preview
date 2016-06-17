fa093d379200ed9312041cf501dedfc0589bfa33
Olivier Bloch <obloch@microsoft.com>
2016-06-17 13:00:18 -0700

    Added device sample to project

---
+++ devicesample/simple_sample_device.js
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+'use strict';
+
+//var Protocol = require('azure-iot-device-amqp').Amqp;
+// Uncomment one of these transports and then change it in fromConnectionString to test other transports
+// var Protocol = require('azure-iot-device-amqp-ws').AmqpWs;
+// var Protocol = require('azure-iot-device-http').Http;
+ var Protocol = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').Client;
+var Message = require('azure-iot-device').Message;
+
+// String containing Hostname, Device Id & Device Key in the following formats:
+//  "HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
+var connectionString = '<connectionstring>';
+// Device Id that will receive the message from IoT Hub
+var deviceId = "<deviceid>";
+
+// fromConnectionString must specify a transport constructor, coming from any transport package.
+var client = Client.fromConnectionString(connectionString, Protocol);
+
+var connectCallback = function (err) {
+  if (err) {
+    console.error('Could not connect: ' + err.message);
+  } else {
+    console.log('Client connected');
+    client.on('message', function (msg) {
+      console.log('Received Message from IoT Hub. Id: ' + msg.messageId + ' Body: ' + msg.data);
+      client.complete(msg, printResultFor('completed'));
+      // reject and abandon follow the same pattern.
+      // /!\ reject and abandon are not available with MQTT
+    });
+
+    // Create a message and send it to the IoT Hub every other second
+    var sendInterval = setInterval(function () {
+      var data = JSON.stringify({ deviceId: deviceId, message: "Hello echo" });
+      var message = new Message(data);
+      console.log('Sending message: ' + message.getData());
+      client.sendEvent(message, printResultFor('send'));
+    }, 2000);
+
+    client.on('error', function (err) {
+      console.error(err.message);
+    });
+
+    client.on('disconnect', function () {
+      clearInterval(sendInterval);
+      client.removeAllListeners();
+      client.connect(connectCallback);
+    });
+  }
+};
+
+client.open(connectCallback);
+
+// Helper function to print results in the console
+function printResultFor(op) {
+  return function printResult(err, res) {
+    if (err) console.log(op + ' error: ' + err.toString());
+    if (res) console.log(op + ' status: ' + res.constructor.name);
+  };
+}
