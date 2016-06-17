043e3c5ff175536f76f1c2838ccdfbea1f122843
Olivier Bloch <obloch@microsoft.com>
2016-06-17 13:01:10 -0700

    Updated function index.js code

--- iothubechofunction/index.js
+++ iothubechofunction/index.js
@@ -4,35 +4,32 @@ var Message = require('azure-iot-common').Message;
 var connectionString = process.env.AzureIoTHubConnectionString;
 
 module.exports = function (context, myEventHubTrigger) {
+
     var client = Client.fromConnectionString(connectionString);
+    
+    client.open(function (err) {
+      if (err) {
+        console.error('Could not connect: ' + err.message);
+      } else {
+        console.log('Client connected');
+    
+        // Create a message and send it to the IoT Hub every second
+      var data = JSON.stringify(myEventHubTrigger.message);
+      var message = new Message(data);
+      console.log('Sending message: ' + message.getData() + 'to: ' + myEventHubTrigger.deviceId);
+      client.send(myEventHubTrigger.deviceId, message, printResultFor('send'));
+      }
+      context.done();
+    });
+}
 
-    if (!myEventHubTrigger) {
-        context.log("Function triggered but without content. Discarding.");
+// Helper function to print results in the console
+function printResultFor(op) {
+  return function printResult(err, res) {
+    if (err) {
+      console.log(op + ' error: ' + err.toString());
+    } else {
+      console.log(op + ' status: ' + res.constructor.name);
     }
-    else {
-        context.log ("Function triggered by device " + myEventHubTrigger.userId + " with following content: " + myEventHubTrigger.getData());
-        context.log('Opening IoT Hub client using connection string ' + connectionString);
-        client.open(function (err) {
-            if (err) {
-                context.log('Error while opening IoT Hub service client: ' + err);
-            } else {
-                context.log('Connected.');
-                var messageToSend = new Message(myEventHubTrigger.getData());
-                context.log('Echoing message back to ' + myEventHubTrigger.userId + ' : ' + messageToSend.getData());
-                client.send(myEventHubTrigger.userId, messageToSend, printResultFor('send', context));
-            }
-        });
-    }
-    context.done();
-};
-
-// Helper function to print results in the context
-function printResultFor(op, context) {
-    return function printResult(err, res) {
-        if (err) {
-            context.log(op + ' error: ' + err.toString());
-        } else {
-            context.log(op + ' status: ' + res.constructor.name);
-        }
-    };
+  };
 }
